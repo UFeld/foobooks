d164f394e6adbe4c7a0db120790dc0b9a6a63bd5
Uri Feld <ufeld@us.ibm.com>
2015-10-13 10:57:04 -0400

    Foobooks update #2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,32 @@ Route::get('/practice', function(){
 echo 'Hello World!';
 
 });
+
+Route::get('/books', function() {
+    return 'Here are all the books...';
+});
+
+Route::get('/books/{category}', function($category) {
+        return 'Here are all the books in the category of '.$category;
+});
+
+Route::get('/new', function() {
+
+    $view  = '<form method="POST">';
+    $view .= csrf_field(); # This will be explained more later
+    $view .= 'Title: <input type="text" name="title">';
+    $view .= '<input type="submit">';
+    $view .= '</form>';
+    return $view;
+
+});
+
+Route::post('/new', function() {
+
+    $input =  Input::all();
+    print_r($input);
+
+});
+
+
+Route::get('/books/show/{title}', 'BookController@getShow');
